1426921274c3442c94aab9e4a2fa89761796e3d0
Jonathan Adithya <jo.adithya391@gmail.com>
2021-05-03 20:44:23 +0700

    add snake functionality (up, down, left, right)

--- snake.py
+++ snake.py
@@ -1,4 +1,8 @@
 from turtle import Turtle
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 
 class Snake:
@@ -17,3 +21,25 @@ class Snake:
             body.setpos(x_pos, 0)
             self.body.append(body)
             x_pos -= 20
+
+    def move(self, ):
+        for index in range(self.length - 1, 0, -1):
+            position = self.body[index - 1].pos()
+            self.body[index].setpos(position)
+        self.head.forward(20)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
