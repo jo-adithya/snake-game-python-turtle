d21bba2df260e60495856feb24ebf1a8cf92e46c
Jonathan Adithya <jo.adithya391@gmail.com>
2021-05-03 20:47:53 +0700

    start the game

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from snake import Snake
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -14,5 +15,11 @@ screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
+
+game_on = True
+while game_on:
+    screen.update()
+    time.sleep(.1)
+    snake.move()
 
 screen.exitonclick()
