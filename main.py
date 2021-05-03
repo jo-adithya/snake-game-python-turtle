a688cac88075f7caaa1b53971b6a172e5f5ced4d
Jonathan Adithya <jo.adithya391@gmail.com>
2021-05-03 20:46:14 +0700

    create the snake object and give its functionality

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from turtle import Screen
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
+
+snake = Snake()
+screen.update()
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+screen.exitonclick()
